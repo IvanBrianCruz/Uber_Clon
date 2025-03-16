c7bf1bb130c36f01595c0fecc99419e9f7fb9345
IvanBrianCruz <ivan4912134@gmail.com>
2025-03-16 20:55:40 -0300

    fin de estilos para las vistas

--- src/main/java/uber/clon/uber/Controllers/ViajeController.java
+++ src/main/java/uber/clon/uber/Controllers/ViajeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import uber.clon.uber.Models.Conductor;
@@ -13,6 +14,7 @@ import uber.clon.uber.Services.ConductorService;
 import uber.clon.uber.Services.ViajeService;
 
 @Controller
+@RequestMapping("/RideOn")
 public class ViajeController {
     private final ViajeService viajeService;
     private final ConductorService conductorService;
